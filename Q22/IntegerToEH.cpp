8ae0738a97acc82a235a252a65e781d5f86e86fe
YYM <979579683@qq.com>
2016-10-19 19:18:25 +0800

    向草爸爸跪下

---
+++ Q22/IntegerToEH.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const static string single[] = {" ","one","two","three","four","five","six","seven","eight","nine"};
+const static string hundred = "hundred";
+const static string ad = "and";
+
+int main () {
+
+}
