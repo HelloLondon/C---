54a41cfb7befd29a4c3c663ba8d4521ad0e3de57
YYM <979579683@qq.com>
2016-10-20 20:05:21 +0800

    爸爸

--- Q19/MaxPro.cpp
+++ Q19/MaxPro.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() {
@@ -8,6 +9,16 @@ int main() {
 	for (int i = 0; i < n ; i ++) {
 		cin >> data[i];
 	}
+//	ofstream out("D:\\C++Project\\Q19\\test.txt",ios::app);
+//	if (out.is_open()) {
+//		out << n << endl;
+//		for (int i = 0; i < n; i ++) {
+//			out << data[i] << " ";
+//		}
+//		out << endl;
+//
+//		out.close();
+//	}
 	long long maxP = data[0];
 	long long cur = 1;
 	for (int i = 0; i < n; i ++) {
